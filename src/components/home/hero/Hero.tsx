34e33564715fcab4d6951ea06338863438ed42a0
Joel Hernandez <jhernandez98@outlook.es>
2024-12-09 06:52:31 -0500

    make responsive hero

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import { BlocksRenderer} from '@strapi/blocks-react-renderer';
 import { leadText, titleHeadline } from '@/config/font.plugin';
 import style from './style.module.css';
+import HeroImage from './ui/hero-image/HeroImage';
 
 interface Props{
   title:string,
@@ -14,26 +14,19 @@ export const Hero = ({title,description,image}:Props) => {
 
   return (
     <section className={style.hero_container}>
-      <div className={style.hero_img_container}>
-        <img
-          width={100}
-          height={100}
-          src={image}
-          alt=""
-         />
-      </div>
+       <HeroImage image= {image}/>
 
       <div className={style.hero_overlay}>
         
        <h1 className={`${titleHeadline.className} ${style.hero_main_headline}`}>
          {title}
-       </h1>
+       </h1> 
 
        <div className={`${leadText.className} ${style.hero_sub_text}`}> 
         <BlocksRenderer content={description}/>
       </div>
       
-      <Link href={'#event-list'} className={`${'btn_cta'} ${titleHeadline.className}`}>Ver Actividades</Link>
+      <Link href={'#event-list'} className={`${'btn_cta'} ${titleHeadline.className} ${style.conocer_btn}`}>Ver Actividades</Link>
       </div>
     </section>
   )
---
+++ src/components/home/hero/ui/hero-image/HeroImage.tsx
@@ -0,0 +1,26 @@
+
+'use client'
+/* eslint-disable @next/next/no-img-element */
+import { useMediaQuery } from "@uidotdev/usehooks";
+import { LARGE_SCREEN_IMAGE } from "@/constants/constant";
+import style from './style.module.css';
+
+interface Props{
+ image:string
+}
+const HeroImage = ({image}:Props) => {
+    const isSmallDevice = useMediaQuery("only screen and (max-width : 767px)");
+
+  return (
+    <div className={style.hero_img_container}>
+    <img
+      width={100}
+      height={100}
+      src={isSmallDevice ? image : LARGE_SCREEN_IMAGE}
+      alt=""
+     />
+  </div>
+  )
+}
+
+export default HeroImage
