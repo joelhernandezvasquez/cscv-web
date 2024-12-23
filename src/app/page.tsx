1e271c0a805f0aefac6d1d2119f98889b602a9e8
Joel Hernandez <jhernandez98@outlook.es>
2024-12-23 17:31:26 -0500

    add story back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,19 @@ import { WeeklyActivities } from "@/components/home/weekly-activities/WeeklyActi
 import { EventList } from "@/components/ui/events-list/EventList";
 import Testimonials from "@/components/ui/testimonials/Testimonials";
 import Ministries from "@/components/ui/ministries/Ministries";
-// import FeatureAnnouncements from "@/components/announcements/FeatureAnnouncements";
+import FeatureAnnouncements from "@/components/announcements/FeatureAnnouncements";
 import Media from "@/components/media/Media";
 import styles from "./page.module.css";
 
 export default async function Home() {
-   const {title,description,image,about,activities,events,testimonies,ministerios} = await getHomeInfo();
+   const {title,description,image,about,activities,events,testimonies,ministerios,anuncios} = await getHomeInfo();
   
    
    return (
       <main className={styles.main}>
          <Hero title={title} description={description} image={image}/>
          <AboutUs about={about}/>
-         {/* <FeatureAnnouncements anuncios={anuncios}/> */}
+         <FeatureAnnouncements anuncios={anuncios}/>
          <EventList events={events}/>
          <WeeklyActivities activities={activities}/> 
          <Ministries ministerios={ministerios}/>
